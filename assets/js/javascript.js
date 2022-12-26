9158c3497b1736ed4f8c8356a577d00780bec4c7
Nadine <n4dinevid4l@hotmail.com>
2022-12-26 08:23:09 +0100

    Footer is complete. Javascript properties placed on the following: - Comment Box - Subscribe Box (in Footer)
    Comments have been placed.

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,8 +1,21 @@
-function changeimage (artisttitle, artistdesc, artistlink, artistbackground)
-{
-    document.getElementById("artisttitle").innerText=artisttitle;
-    document.getElementById("artistdesc").innerText=artistdesc;
-    document.getElementById("artistlink").href=artistlink;
-    document.getElementById("artistbackground").style.backgroundImage='url("' + artistbackground + '")';
+function changeimage(artisttitle, artistdesc, artistlink, artistbackground) {
+    document.getElementById("artisttitle").innerText = artisttitle;
+    document.getElementById("artistdesc").innerText = artistdesc;
+    document.getElementById("artistlink").href = artistlink;
+    document.getElementById("artistbackground").style.backgroundImage = 'url("' + artistbackground + '")';
 
+}
+
+function subscribeSuccess() {
+    document.getElementById("subscribetext").classList.add("hide");
+    document.getElementById("subscribebtn").classList.add("hide");
+    document.getElementById("subscribeoutput").classList.remove("hide");
+}
+
+function commentbox() {
+    document.getElementById("commentfullname").setAttribute("disabled", "");
+    document.getElementById("commentemail").setAttribute("disabled", "");
+    document.getElementById("commentbox").setAttribute("disabled", "");
+    document.getElementById("commentbtn").setAttribute("disabled", "");
+    document.getElementById("commentbtn").innerText = "Comment Sent!";
 }
